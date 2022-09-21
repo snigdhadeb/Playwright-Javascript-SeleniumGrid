a4e003712e56c8bf9a99368efd85d032e06682c9
Snigdhadeb <snigdha98.3@gmail.com>
2022-09-21 20:59:26 +0530

    first commit

---
+++ playwright.config.js
@@ -0,0 +1,110 @@
+// @ts-check
+const { devices } = require('@playwright/test');
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// require('dotenv').config();
+
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ * @type {import('@playwright/test').PlaywrightTestConfig}
+ */
+const config = {
+  testDir: './tests',
+  /* Maximum time one test can run for. */
+  timeout: 30 * 1000,
+  expect: {
+    /**
+     * Maximum time expect() should wait for the condition to be met.
+     * For example in `await expect(locator).toHaveText();`
+     */
+    timeout: 5000
+  },
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: 'html',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
+    actionTimeout: 0,
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    // baseURL: 'http://localhost:3000',
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry',
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+        //headless:false,
+        screenshot: `on`,
+        video: `on`,
+        contextOptions: {recordVideo: { dir: "./videos"}},
+        trace: `on`,
+      },
+    },
+    /* {
+      name: `Chrome`,
+      use: {
+        browserName: `chromium`,
+        channel: `chrome`,
+        headless: false,
+        viewport: { width: 1720, height: 850 },
+        screenshot: `only-on-failure`,
+        video: `retain-on-failure`,
+        contextOptions: {recordVideo: { dir: "./videos"}},
+        trace: `retain-on-failure`,
+      },
+    }, */
+    /* {
+      name: `Firefox`,
+      use: {
+        browserName: `firefox`,
+        viewport: { width: 1720, height: 850 },
+        ignoreHTTPSErrors: true,
+        screenshot: `only-on-failure`,
+        video: `retain-on-failure`,
+        trace: `retain-on-failure`,
+        launchOptions: {
+          slowMo: 200,
+        },
+      },
+    },
+    {
+      name: `Safari`,
+      use: {
+        browserName: `webkit`,
+        viewport: { width: 1720, height: 850 },
+        ignoreHTTPSErrors: true,
+        screenshot: `only-on-failure`,
+        video: `retain-on-failure`,
+        trace: `retain-on-failure`,
+      },
+    }, */
+  ],
+
+  /* Folder for test artifacts such as screenshots, videos, traces, etc. */
+  // outputDir: 'test-results/',
+
+  /* Run your local dev server before starting the tests */
+  // webServer: {
+  //   command: 'npm run start',
+  //   port: 3000,
+  // },
+};
+
+module.exports = config;
